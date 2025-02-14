d7e532abdb3c2a23fee4f96bb1c09f83be7c99ea
JoJinyoung <68494100+Thedume@users.noreply.github.com>
2025-02-14 09:14:43 +0900

    상세정보 검색 추가 & 도서 리스트를 단일 데이터로 수정

--- BE-MyBookShelf/src/main/java/org/wsp/mybookshelf/global/searchApi/controller/ApiController.java
+++ BE-MyBookShelf/src/main/java/org/wsp/mybookshelf/global/searchApi/controller/ApiController.java
@@ -1,5 +1,6 @@
 package org.wsp.mybookshelf.global.searchApi.controller;
 
+import org.wsp.mybookshelf.global.searchApi.dto.BookResponse;
 import org.wsp.mybookshelf.global.searchApi.service.AladinService;
 import org.wsp.mybookshelf.global.searchApi.service.GoogleBooksService;
 import org.wsp.mybookshelf.global.searchApi.service.LibraryService;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -24,21 +26,20 @@ public class ApiController {
 
     // 알라딘 OpenAPI만 사용한 코드
     @GetMapping("/search")
-    public Mono<Map<String, Object>> searchBooks(
-            @RequestParam(required = true) String query,
-            @RequestParam(required = false, defaultValue = "Keyword") String queryType,
-            /*
-            Keyword (기본값) : 제목 + 저자
-            Title : 제목
-            Author : 저자
-            Publisher : 출판사
-            */
-            @RequestParam(required = true, defaultValue = "1") String start
+    public Mono<Map<@org.jetbrains.annotations.NotNull String, @org.jetbrains.annotations.NotNull List<BookResponse>>> searchBooks(
+            @RequestParam String query,
+            @RequestParam(defaultValue = "Keyword") String queryType,
+            @RequestParam(defaultValue = "1") String start,
+            @RequestParam(defaultValue = "6") int maxResults,
+            @RequestParam(defaultValue = "true") boolean showDetail
     ) {
-        return Mono.fromCallable(() -> aladinService.searchBooks(query, queryType, start))
-                .subscribeOn(Schedulers.boundedElastic())
-                .map(aladinBooks -> Map.of("aladinAPI", aladinBooks));
+        return Mono.fromCallable(() -> {
+            //List<BookResponse> books = aladinService.searchBooks(query, queryType, start, maxResults, showDetail);
+            List<BookResponse> books = LibraryService.searchBooks(query);
+            return Map.of("books", books);
+        }).subscribeOn(Schedulers.boundedElastic());
     }
+
 
     // 알라딘 + GoogleAPI
 //    @GetMapping("/search")
--- BE-MyBookShelf/src/main/java/org/wsp/mybookshelf/global/searchApi/dto/BookResponse.java
+++ BE-MyBookShelf/src/main/java/org/wsp/mybookshelf/global/searchApi/dto/BookResponse.java
@@ -7,33 +7,14 @@ import java.util.List;
 
 @Data
 public class BookResponse {
-    private List<Book> books;
-
-    @Data
-    public static class Book {
-        private String title;
-        private String author;
-        private String publisher;
-        private String isbn;
-        private String publicationDate;  // 출판일 추가
-        private String Cover; // 책 표지
-        private Integer customerReviewRank;
-        private String source;  // 데이터 출처
-    }
-
-    public void sortBooks(String sortBy) {
-        if (books == null) return;
-
-        switch (sortBy) {
-            case "title":
-                books.sort(Comparator.comparing(Book::getTitle));
-                break;
-            case "publisher":
-                books.sort(Comparator.comparing(Book::getPublicationDate, Comparator.nullsLast(Comparator.reverseOrder())));
-                break;
-            case "source":
-                books.sort(Comparator.comparing(Book::getSource));
-                break;
-        }
-    }
+    private String title;
+    private String author;
+    private String publisher;
+    private String genre;
+    private String isbn;
+    private String publicationDate;  // 출판일 추가
+    private String Cover; // 책 표지
+    private Integer customerReviewRank;
+    private String source;  // 데이터 출처
+    private String description;
 }
